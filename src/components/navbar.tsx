17b7c56b7b5df5ee0cb16dad46360c828578e8a0
akaash.walker <awalker7179@gmail.com>
2025-07-16 11:35:57 -0400

    add shadcn component library

---
+++ src/components/navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+    return (
+        <div>
+            <img src="/IconOnly_Transparent_NoBuffer.png" alt="Logo" width="75" height="75"/>
+        </div>
+    );
+}
