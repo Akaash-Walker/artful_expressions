8983d5d77c881eadc8ee8d5cc3ad72ae328c2304
Akaash-Walker <awalker7179@gmail.com>
2025-07-17 12:34:02 -0400

    fix shadcn component library issue

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,6 +2,7 @@ export default function Navbar() {
     return (
         <div>
             <img src="/IconOnly_Transparent_NoBuffer.png" alt="Logo" width="75" height="75"/>
+
         </div>
     );
 }
