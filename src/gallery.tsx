12b4bbd2cc5a907ecfc815fb99746dd806dd190e
akaash.walker <awalker7179@gmail.com>
2025-07-25 12:03:45 -0400

    add example images to carousel in gallery

--- src/gallery.tsx
+++ src/gallery.tsx
@@ -2,29 +2,46 @@ import {Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious}
 import {Card} from "@/components/ui/card.tsx";
 import Banner from "@/components/banner.tsx";
 
+import Autoplay from "embla-carousel-autoplay"
+
+
 export default function Gallery() {
     return (
         <div>
             <Banner title={"Gallery"}/>
-            <Carousel className="w-2/3 mx-auto">
-                <CarouselContent>
-                    <CarouselItem className={"basis-1/3"}>
-                        <Card className="p-8"><img src={"/girlpaint.jpg"}/></Card>
-                    </CarouselItem>
-                    <CarouselItem className={"basis-1/3"}>
-
-                        <Card className="p-8"><img src={"/hero.jpg"}/></Card>
-                    </CarouselItem>
-                    <CarouselItem className={"basis-1/3"}>
-                        <Card className="p-8"><img src={"/FullLogo_Transparent_NoBuffer.png"}/></Card>
-                    </CarouselItem>
-                    <CarouselItem className={"basis-1/3"}>
-                        <Card className="p-8"><img src={"/IconOnly_Transparent_NoBuffer.png"}/></Card>
-                    </CarouselItem>
-                </CarouselContent>
-                <CarouselPrevious/>
-                <CarouselNext/>
-            </Carousel>
+            <div>
+                <Carousel className="md:w-2/3 mx-auto" orientation={"horizontal"} plugins={[
+                    Autoplay({
+                        delay: 3000,
+                        stopOnInteraction: false,
+                        stopOnMouseEnter: true,
+                        playOnInit: true
+                    }),
+                ]}>
+                    <CarouselContent>
+                        <CarouselItem className={"md:basis-1/3"}>
+                            <Card className="p-8"><img src={"/stock_paintings/painting (1).jpg"}/></Card>
+                        </CarouselItem>
+                        <CarouselItem className={"md:basis-1/3"}>
+                            <Card className="p-8"><img src={"/stock_paintings/painting (1).avif"}/></Card>
+                        </CarouselItem>
+                        <CarouselItem className={"md:basis-1/3"}>
+                            <Card className="p-8"><img src={"/stock_paintings/painting (1).png"}/></Card>
+                        </CarouselItem>
+                        <CarouselItem className={"md:basis-1/3"}>
+                            <Card className="p-8"><img src={"/stock_paintings/painting (1).webp"}/></Card>
+                        </CarouselItem>
+                        <CarouselItem className={"md:basis-1/3"}>
+                            <Card className="p-8"><img src={"/stock_paintings/painting (2).avif"}/></Card>
+                        </CarouselItem>
+                        <CarouselItem className={"md:basis-1/3"}>
+                            <Card className="p-8"><img src={"/stock_paintings/painting (2).jpg"}/></Card>
+                        </CarouselItem>
+                    </CarouselContent>
+                    <CarouselPrevious/>
+                    <CarouselNext/>
+                </Carousel>
+            </div>
         </div>
     )
 }
