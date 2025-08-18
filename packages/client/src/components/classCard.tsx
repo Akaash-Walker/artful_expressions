bf2a1f3cfefea658ffa641400c0a0a67daa104c7
Akaash-Walker <awalker7179@gmail.com>
2025-08-17 21:09:05 -0400

    refactor classes.tsx to use classCard.tsx

---
+++ packages/client/src/components/classCard.tsx
@@ -0,0 +1,34 @@
+import Heading from "./heading.js";
+import {Label} from "./ui/label.js";
+import {Button} from "./ui/button.js";
+import {useNavigate} from "react-router-dom";
+
+interface ClassCardProps {
+    title: string;
+    description1: string;
+    features?: string[];
+    description2?: string;
+}
+
+export default function ClassCard({title, description1, features, description2}: ClassCardProps) {
+    const navigate = useNavigate();
+    return (
+        <div className="space-y-4">
+            <Heading title={title}/>
+            <Label className="text-md leading-normal">
+                {description1}
+            </Label>
+            <ul className="list-disc pl-5 mt-2">
+                {features?.map((feature, index) => (
+                    <li key={index}>{feature}</li>
+                ))}
+            </ul>
+            <Label className="text-md font-normal leading-normal">
+                {description2}
+            </Label>
+            <Button className="mt-2" onClick={() => navigate("/bookings")}>
+                Book Now
+            </Button>
+        </div>
+    )
+}
