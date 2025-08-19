e47d66ee4f0a3996fa66b7eb7907a86ca5bf8b6a
Akaash-Walker <awalker7179@gmail.com>
2025-08-18 22:07:57 -0400

    update gallery

--- packages/client/src/components/heading.tsx
+++ packages/client/src/components/heading.tsx
@@ -2,11 +2,12 @@ import {Label} from "./ui/label.tsx";
 
 interface HeadingProps {
     title: string;
+    className?: string;
 }
 
-export default function Heading({title}: HeadingProps) {
+export default function Heading({title, className}: HeadingProps) {
     return (
-        <Label className={"mt-16 pb-8 text-2xl md:text-3xl font-medium text-[var(--main-red)]"}>
+        <Label className={"mt-16 pb-8 text-2xl md:text-3xl font-medium text-[var(--main-red)] " + (className ?? "") }>
             {title}
         </Label>
     )
--- packages/client/src/gallery.tsx
+++ packages/client/src/gallery.tsx
@@ -3,16 +3,20 @@ import {Card} from "./components/ui/card.tsx";
 import Banner from "./components/banner.tsx";
 
 import Autoplay from "embla-carousel-autoplay"
+import Heading from "./components/heading.tsx";
 
 
 export default function Gallery() {
     return (
         <div>
             <Banner title={"Gallery"}/>
-            <div>
-                <Carousel className="md:w-2/3 mx-auto" orientation={"horizontal"} plugins={[
+            <div className={"text-center"}>
+                <Heading title={"Featured"} className={"w-full justify-center text-center"}/>
+            </div>
+            <div className={"mb-24"}>
+                <Carousel className="md:w-4/5 mx-auto" orientation={"horizontal"} plugins={[
                     Autoplay({
-                        delay: 3000,
+                        delay: 2000,
                         stopOnInteraction: false,
                         stopOnMouseEnter: true,
                         playOnInit: true
@@ -20,27 +24,40 @@ export default function Gallery() {
                 ]}>
                     <CarouselContent>
                         <CarouselItem className={"md:basis-1/3"}>
-                            <Card className="p-8"><img src={"/stock_paintings/painting (1).jpg"}/></Card>
+                            <Card className="p-8"><img src={"/girlpaint.jpg"} alt="Featured image 1"/></Card>
                         </CarouselItem>
                         <CarouselItem className={"md:basis-1/3"}>
-                            <Card className="p-8"><img src={"/stock_paintings/painting (1).avif"}/></Card>
+                            <Card className="p-8"><img src={"/girlpaint.jpg"} alt="Featured image 2"/></Card>
                         </CarouselItem>
                         <CarouselItem className={"md:basis-1/3"}>
-                            <Card className="p-8"><img src={"/stock_paintings/painting (1).png"}/></Card>
+                            <Card className="p-8"><img src={"/girlpaint.jpg"} alt="Featured image 3"/></Card>
                         </CarouselItem>
                         <CarouselItem className={"md:basis-1/3"}>
-                            <Card className="p-8"><img src={"/stock_paintings/painting (1).webp"}/></Card>
+                            <Card className="p-8"><img src={"/girlpaint.jpg"} alt="Featured image 4"/></Card>
                         </CarouselItem>
                         <CarouselItem className={"md:basis-1/3"}>
-                            <Card className="p-8"><img src={"/stock_paintings/painting (2).avif"}/></Card>
+                            <Card className="p-8"><img src={"/girlpaint.jpg"}/></Card>
                         </CarouselItem>
                         <CarouselItem className={"md:basis-1/3"}>
-                            <Card className="p-8"><img src={"/stock_paintings/painting (2).jpg"}/></Card>
+                            <Card className="p-8"><img src={"/girlpaint.jpg"}/></Card>
                         </CarouselItem>
                     </CarouselContent>
                     <CarouselPrevious/>
                     <CarouselNext/>
                 </Carousel>
+            </div>
+            <div className={"flex-grid w-4/5 lg:w-2/3 mx-auto mb-24"}>
+                <Heading title={"More Images"}/>
+                <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 items-start">
+                    {Array.from({ length: 12 }).map((_, idx) => (
+                        <img
+                            key={idx}
+                            src={"/girlpaint.jpg"}
+                            className="w-full h-auto rounded-lg mb-4"
+                            alt={`Gallery test ${idx + 1}`}
+                        />
+                    ))}
+                </div>
             </div>
         </div>
     )
